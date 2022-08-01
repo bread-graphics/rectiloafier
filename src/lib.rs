db11b7decdd114531440a609722a14943b5e2e6c
jtnunley <jtnunley01@gmail.com>
2022-08-01 08:46:54 -0700

    add box conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
-// This file is part of bentley-ottmann.
+// This file is part of rectiloafier.
 //
-// bentley-ottmann is free software: you can redistribute it and/or
+// rectiloafier is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation,
 // either version 3 of the License, or (at your option)
 // any later version.
 //
-// bentley-ottmann is distributed in the hope that it will be useful,
+// rectiloafier is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General
-// Public License along with bentley-ottmann. If not, see
+// Public License along with rectiloafier. If not, see
 // <https://www.gnu.org/licenses/>.
 
 #![no_std]
 #![forbid(unsafe_code, rust_2018_idioms)]
 
-use geometry::{PathEvent, Point2D, Scalar};
+use bentley_ottmann::trapezoids;
+use core::{convert::TryInto, iter::FusedIterator};
+use geometry::{Box2D, Edge, FillRule, PathEvent, Point2D, Scalar};
+use num_traits::Bounded;
 
 /// Tell whether or not a path is rectilinear.
 pub fn is_rectilinear<Num: Scalar>(
@@ -39,6 +42,25 @@ pub fn is_rectilinear<Num: Scalar>(
             approx_eq(diff.x, Num::ZERO) || approx_eq(diff.y, Num::ZERO)
         }
     })
+}
+
+/// Tell whether or not a polygon is rectilinear.
+pub fn is_polygon_rectilinear<Num: Scalar>(polygon: impl IntoIterator<Item = Edge<Num>>) -> bool {
+    polygon.into_iter().all(|edge| {
+        approx_eq(edge.line.vector.y, Num::ZERO) || approx_eq(edge.line.vector.x, Num::ZERO)
+    })
+}
+
+/// Get the rectangles making up a rectilinear path.
+///
+/// # Panics
+///
+/// Panics if the path is not rectilinear.
+pub fn boxes<Num: Scalar + Bounded + Default>(
+    polygon: impl IntoIterator<Item = Edge<Num>>,
+) -> impl FusedIterator<Item = Box2D<Num>> {
+    trapezoids(polygon, FillRule::Winding)
+        .map(|trap| trap.try_into().expect("path is not rectilinear"))
 }
 
 fn approx_eq<Num: Scalar>(a: Num, b: Num) -> bool {
